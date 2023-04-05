227d051daeffa642590c277df7c3b3d4bb9c77f8
doailabs <128745776+doailabs@users.noreply.github.com>
2023-04-05 18:10:21 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,11 @@
+// Initialize
+function init() {
+  // Llama a registerEventHandlers cuando el DOM esté completamente cargado
+  document.addEventListener('DOMContentLoaded', () => {
+    registerEventHandlers();
+    registerToggleWrapUpIdsFieldHandler();
+  });
+}
 
+// Llama a la función init
+init();
