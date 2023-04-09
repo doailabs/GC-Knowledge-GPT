314348534191e98b3a3379618d487294dbc1ef72
doailabs <128745776+doailabs@users.noreply.github.com>
2023-04-09 20:00:38 +0200

    Update api.js

--- api.js
+++ api.js
@@ -144,6 +144,65 @@ async function createRow(dataTableId, rowData) {
   }
 }
 
+async function findDatatableRow(datatableId, knowledgeBaseId) {
+  try { 
+    const architectApi = new platformClient.ArchitectApi();
+    const opts = {
+      'showbrief': false
+    };
+
+    const response = await architectApi.getFlowsDatatableRows(datatableId, opts);
+    const row = response.entities.find(row => row.key === knowledgeBaseId);
+    return row;
+  } catch (error) {
+    console.error(`Error finding datatable row with key '${knowledgeBaseId}':`, error);
+  }
+}
+
+async function updateConfiguration(datatableId, rowId, config) {
+  try {
+    const architectApi = new platformClient.ArchitectApi();
+    const response = await architectApi.putFlowsDatatableRow(datatableId, rowId, config);
+    return response;
+  } catch (error) {
+    console.error('Error updating configuration:', error);
+  }
+}
+
+function saveNewConfiguration() {
+  const architectApi = new platformClient.ArchitectApi();
+  const newConfig = {
+    knowledge_base_id: document.getElementById('knowledgeBaseIdNew').value,
+    language: document.getElementById('language').value,
+    min_answer_confidence: document.getElementById('minAnswerConfidence').value,
+    system_prompt: document.getElementById('systemPrompt').value,
+    no_match_behavior: document.getElementById('noMatchBehavior').value,
+    create_knowledge_articles: document.getElementById('createKnowledgeArticles').checked,
+    wrap_up_ids: document.getElementById('wrapUpIds').value,
+    model: document.getElementById('model').value,
+    temperature: document.getElementById('temperature').value,
+    max_tokens: document.getElementById('maxTokens').value
+  };
+
+  findDataTable()
+    .then((dataTableId) => {
+      if (dataTableId) {
+        const row = {
+          key_value_data: newConfig
+        };
+        return architectApi.postFlowsDatatableRows(dataTableId, row);
+      } else {
+        throw new Error('Data table not found.');
+      }
+    })
+    .then((response) => {
+      console.log('New configuration saved:', response);
+    })
+    .catch((error) => {
+      console.error('Error saving new configuration:', error);
+    });
+}
+
 
 
 
