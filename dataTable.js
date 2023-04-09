26b34a6eb3d9c0e812e9cded2ba5630f803ab8f1
doailabs <128745776+doailabs@users.noreply.github.com>
2023-04-09 23:22:01 +0200

    Update dataTable.js

--- dataTable.js
+++ dataTable.js
@@ -31,6 +31,29 @@ async function insertConfigurationRow(dataTableId, knowledgeBaseId, systemPrompt
   }  
 }
 
+async function updateConfigurationRow(dataTableId, knowledgeBaseId, systemPrompt, language, minAnswerConfidence, noMatchBehavior, createKnowledgeArticles, wrapUpIds, model, temperature, maxTokens) {
+  const newRow = {
+    "Temperature": parseFloat(temperature),
+    "Minimum Answer Confidence": parseFloat(minAnswerConfidence),
+    "Create knowledge articles based on wrap ups": createKnowledgeArticles,
+    "Language": language,
+    "MaxTokens": parseInt(maxTokens, 10),
+    "Model": model,
+    "No Match Behavior": noMatchBehavior,
+    "key": knowledgeBaseId,
+    "System Prompt": systemPrompt,
+    "Wrap up ids for knowledge articles": wrapUpIds
+  };
+  
+  try {
+    await updateRow(dataTableId, newRow);
+    return true;
+  } catch (error) {
+    console.error('Error al llamar a updateRow desde updateConfigurationRow: ', error);
+    return false;
+  }  
+}
+
 
 
 
