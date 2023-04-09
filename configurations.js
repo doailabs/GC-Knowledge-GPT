95ef1260db2679c1ad0baa471ba11bccaf601161
doailabs <128745776+doailabs@users.noreply.github.com>
2023-04-09 23:52:08 +0200

    Update configurations.js

--- configurations.js
+++ configurations.js
@@ -50,11 +50,11 @@ function displayConfiguration(knowledgeBaseId) {
         newConfigurationSection.style.display = 'none';
         document.getElementById('knowledgeBaseIdExisting').value = rowData.key;
         document.getElementById('language').value = rowData.Language;
-        document.getElementById('minAnswerConfidence').value = rowData.Minimum Answer Confidence;
-        document.getElementById('systemPrompt').value = rowData.System Prompt;
-        document.getElementById('noMatchBehavior').value = rowData.No Match Behavior;
-        document.getElementById('createKnowledgeArticles').checked = rowData.Create knowledge articles based on wrap ups;
-        document.getElementById('wrapUpIds').value = rowData.Wrap up ids for knowledge articles;
+        document.getElementById('minAnswerConfidence').value = rowData."Minimum Answer Confidence";
+        document.getElementById('systemPrompt').value = rowData."System Prompt";
+        document.getElementById('noMatchBehavior').value = rowData."No Match Behavior";
+        document.getElementById('createKnowledgeArticles').checked = rowData."Create knowledge articles based on wrap ups";
+        document.getElementById('wrapUpIds').value = rowData."Wrap up ids for knowledge articles";
         document.getElementById('model').value = rowData.Model;
         document.getElementById('temperature').value = rowData.Temperature;
         document.getElementById('maxTokens').value = rowData.MaxTokens;
