c7a442317656ac2d5f529f2d1b81708aaadffdc8
doailabs <128745776+doailabs@users.noreply.github.com>
2023-04-10 09:24:55 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,17 +1,15 @@
 // Initialize
 function init() {
     startGCSDKs(clientId).then(() => {
-      registerEventHandlers();
+      document.addEventListener('DOMContentLoaded', () => {
+          registerEventHandlers();
+          registerToggleWrapUpIdsFieldHandler();
+      });
     });
 
     document.getElementById('knowledgeBasesTable').addEventListener('change', function (event) {
       const selectedKnowledgeBaseId = event.target.value;
       document.getElementById('knowledgeBaseIdNew').value = selectedKnowledgeBaseId;
       displayConfiguration(selectedKnowledgeBaseId);
-    });
-
-    document.addEventListener('DOMContentLoaded', () => {
-      registerEventHandlers();
-      registerToggleWrapUpIdsFieldHandler();
     });
 }
